8261247b7986adc6a9ba4674d0694d01fc74517c
Rayian Mahi <rbsmahi@gmail.com>
2022-06-26 16:36:35 +0600

    conflict resolution

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,23 @@
 </script>
 
 <template>
-  <div class="container mx-auto my-5">
-    <el-main>
+  <main class="container mx-auto lg:my-14">
+    <div class="container">
       <h1 class="text-6xl mb-8">Welcome to Amar Progress!</h1>
+      <div class="alert alert-info shadow-lg mt-4 mb-6">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            class="stroke-current flex-shrink-0 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+          </svg>
+          <span>New progress dashboard is coming soon!</span>
+        </div>
+      </div>
       <p class="text-xl ">
         This is a progress tracking app for students.<br />
         Get started by making a new account!
-
-        <span>A new dashboard is coming!</span>
       </p>
-    </el-main>
-  </div>
+    </div>
+  </main>
 </template>
