35f64a8780bbb71a698e70d38f1b35f8a2efae8b
Rayian Mahi <rbsmahi@gmail.com>
2022-07-02 04:12:00 +0600

    changed some padding

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -64,7 +64,7 @@ onMounted(async () => {
     if (store.state.user == null) {
         router.push('/')
     }
-    await fetchAllData();
+    // await fetchAllData();
 })
 
 
@@ -73,11 +73,11 @@ onMounted(async () => {
     <div class="container mx-auto my-5">
         <el-main v-if="hasLoaded">
             <h2 class="text-3xl my-5">Overall Stats</h2>
-            <div class="mx-4">
+            <div class="md:mx-4">
                 <subject-progress-card title="Overall Progress" fluid description="lorem5" :progress="30" />
             </div>
 
-            <h2 class="text-3xl my-5">Basic Subjects Progress</h2>
+            <h2 class="text-3xl my-5">Basic Subjects</h2>
             <div
                 class="my-12 lg:my-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-y-6 md:gap-x-4 md:justify-items-center">
                 <subject-progress-card title="Bengali 1st Paper" description="lorem5" :progress="65" />
@@ -86,7 +86,7 @@ onMounted(async () => {
                 <subject-progress-card title="English 2nd Paper" description="lorem5" :progress="65" />
             </div>
 
-            <h2 class="text-3xl mb-5 mt-10">Science Subjects Progress</h2>
+            <h2 class="text-3xl mb-5 mt-10">Science Subjects</h2>
             <div
                 class="my-12 lg:my-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-y-6 md:gap-x-4 md:justify-items-center">
                 <subject-progress-card title="Physics 1st Paper" description="lorem5" :progress="65" planned="8" MCQ="3"
